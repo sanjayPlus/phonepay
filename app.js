776ec6d8d9831d93c5c51943b924cbf9da3f0e72
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2023-12-28 16:37:37 +0530

    redirect added

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const axios = require('axios');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/phonepay')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Error connecting to MongoDB:', err));
+
+const phonePaySchema = new mongoose.Schema({
+  data:Object,
+  name:String,
+  phone:String
+})
+const PhonePay = mongoose.model('PhonePay',phonePaySchema);
 // Load environment variables from .env file
 dotenv.config();
 
@@ -39,7 +52,7 @@ app.get('/checkout/:amount/:name/:phone', async (req, res) => {
         merchantUserId:"MUID" + Date.now(),
         name:  req.params.name,
         amount: req.params.amount * 100,
-        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId,
+        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId+"/"+process.env.MERCHANT_ID+"/"+req.params.name+"/"+req.params.phone,
         redirectMode: 'POST',
         mobileNumber: req.params.phone,
         paymentInstrument: {
@@ -53,7 +66,7 @@ app.get('/checkout/:amount/:name/:phone', async (req, res) => {
     const sha256 = crypto.createHash('sha256').update(string).digest('hex');
     const checksum = sha256 + '###' + keyIndex;
 
-    const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+    const prod_URL = process.env.API_URL+"/pg/v1/pay"
     const options = {
         method: 'POST',
         url: prod_URL,
@@ -91,7 +104,7 @@ app.get('/payment/:amount/:name/:phone', async (req, res) => {
         merchantUserId:"MUID" + Date.now(),
         name:  req.params.name,
         amount: req.params.amount * 100,
-        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId,
+        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId+"/"+process.env.MERCHANT_ID+"/"+req.params.name+"/"+req.params.phone,
         redirectMode: 'POST',
         mobileNumber: req.params.phone,
         paymentInstrument: {
@@ -105,7 +118,7 @@ app.get('/payment/:amount/:name/:phone', async (req, res) => {
     const sha256 = crypto.createHash('sha256').update(string).digest('hex');
     const checksum = sha256 + '###' + keyIndex;
 
-    const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+    const prod_URL = process.env.API_URL+"/pg/v1/pay"
     const options = {
         method: 'POST',
         url: prod_URL,
@@ -135,9 +148,55 @@ app.get('/payment/:amount/:name/:phone', async (req, res) => {
 }
 });
 
+app.get('/status/:transactionId/:merchantId/:name/:phone', async (req, res) => {
+  const merchantTransactionId = req.params.transactionId
+    const merchantId = req.params.merchantId
+    const  name =  req.params.name;
+    const  phone =  req.params.phone;
+    console.log(name,phone)
+    const keyIndex = 1;
+    const string = `/pg/v1/status/${merchantId}/${merchantTransactionId}` + process.env.PHONEPAY_API_KEY;
+    const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+    const checksum = sha256 + "###" + keyIndex;
 
+    const options = {
+    method: 'GET',
+    url: `${process.env.API_URL}/pg/v1/status/${merchantId}/${merchantTransactionId}`,
+    headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-VERIFY': checksum,
+        'X-MERCHANT-ID': `${merchantId}`
+    }
+    };
 
-
+    // CHECK PAYMENT TATUS
+    axios.request(options).then(async(response) => {
+        if (response.data.success === true) {
+          const phonePay = new PhonePay({
+            data:response.data.data,
+            name:name,
+            phone:phone
+          })
+          await phonePay.save()
+            
+            const url = `${process.env.REDIRECT_URL}/success`
+            return res.redirect(url)
+        } else {
+            const url = `${process.env.REDIRECT_URL}/failure`
+            return res.redirect(url)
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+})
+app.get("/success", (req, res) => {
+  res.send("Payment Successful");
+})
+app.get("/failure", (req, res) => {
+  res.send("Payment Failed");
+})
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
