e3e42bbf038915071c5a6dd5e0475f3150da9e36
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2023-12-28 15:41:02 +0530

    new things added

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/checkout/:amount/:name/:phone', async (req, res) => {
         merchantUserId:"MUID" + Date.now(),
         name:  req.params.name,
         amount: req.params.amount * 100,
-        redirectUrl: process.env.REDIRECT_URL,
+        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId,
         redirectMode: 'POST',
         mobileNumber: req.params.phone,
         paymentInstrument: {
@@ -82,6 +82,62 @@ app.get('/checkout/:amount/:name/:phone', async (req, res) => {
     })
 }
 });
+app.get('/payment/:amount/:name/:phone', async (req, res) => {
+  try {
+    const merchantTransactionId = crypto.randomBytes(16).toString('hex');
+    const data = {
+        merchantId: process.env.MERCHANT_ID,
+        merchantTransactionId: merchantTransactionId,
+        merchantUserId:"MUID" + Date.now(),
+        name:  req.params.name,
+        amount: req.params.amount * 100,
+        redirectUrl: process.env.REDIRECT_URL+"/status/" + merchantTransactionId,
+        redirectMode: 'POST',
+        mobileNumber: req.params.phone,
+        paymentInstrument: {
+            type: 'PAY_PAGE'
+        }
+    };
+    const payload = JSON.stringify(data);
+    const payloadMain = Buffer.from(payload).toString('base64');
+    const keyIndex = 1;
+    const string = payloadMain + '/pg/v1/pay' + process.env.PHONEPAY_API_KEY;
+    const sha256 = crypto.createHash('sha256').update(string).digest('hex');
+    const checksum = sha256 + '###' + keyIndex;
+
+    const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+    const options = {
+        method: 'POST',
+        url: prod_URL,
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-VERIFY': checksum
+        },
+        data: {
+            request: payloadMain
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data)
+        return res.status(200).json(response.data.data.instrumentResponse.redirectInfo.url);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+
+} catch (error) {
+    res.status(500).send({
+        message: error.message,
+        success: false
+    })
+}
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
